6f77ee919c9504a4cb370ece3fc3ba58ba40e0bb
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-11 09:02:26 +1000

    hi

--- project/instancecreate/create.sh
+++ project/instancecreate/create.sh
@@ -8,6 +8,7 @@ LVER=2
 if [ -z "${COMPONENT}" ]; then
 
   echo "COMPONENT name is needed"
+  exit 1
 
 fi
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
