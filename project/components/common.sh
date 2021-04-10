92f8168863aa2df88b11087ce9558f8b72ad64f1
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-10 16:10:11 +1000

    Hi this my first programme

--- project/components/common.sh
+++ project/components/common.sh
@@ -5,3 +5,21 @@ if [ "${USER_ID}" -ne 0 ]; then
   echo -e "\e[1;31mYou should be a root user to perform this command\e[0m"
   exit 1
 fi
+PRINT(){
+
+echo "........................................................................"
+echo -e "\e[1;35m [info] intsalling nginx\e[om"
+echo "........................................................................."
+}
+
+STAT(){
+yum install nginx -y
+if [ $1 -ne 0 ]; then
+echo "........................................................................."
+echo -e "\e[1;31m failure of nginx\e[om"
+exit 2
+else
+echo "........................................................................."
+echo -e "\e[1;32m success of nginx\e[om"
+fi
+}
--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
+source common.sh
 
-echo -e "\e[1;35m [info] intsalling nginx\e[om"
 
+PRINT "INTSALL NGINX"
 yum install nginx -y
-if [ $? -ne 0 ]; then
-
-echo -e "\e[1;31m failure of nginx\e[om"
-
-else
-echo -e "\e[1;32m success of nginx\e[om"
-exit 2
-
-fi
+STAT $? " NGINX INSTALLATION "
