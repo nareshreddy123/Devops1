2e6267cc53fd958b245746835077636a703459b9
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-10 15:16:14 +1000

    Hi this my first programme

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
- yum install nginxccc -y
+ yum install nginx  -y
 
 if [ $? -ne 0 ]; then
-
-  echo -e "\e[31m failure of nginx\e[om"
-
-  exit 2
+echo -e "\e[31m failure of nginx\e[om"
+exit 2
   fi
