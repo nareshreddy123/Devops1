a7209907bce85e60efaab9b6e63ae1ac2e0c1f1d
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-10 15:43:34 +1000

    Hi this my first programme

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
- yum install nginx  -y
 
+echo -e "\e[1;35m [info] intsalling nginx\e[om"
+yum install nginx  -y
 if [ $? -ne 0 ]; then
-echo -e "\e[31m failure of nginx\e[om"
+echo -e "\e[1;31m failure of nginx\e[om"
+else
+echo -e "\e[1;32m success of nginx\e[om"
 exit 2
   fi
