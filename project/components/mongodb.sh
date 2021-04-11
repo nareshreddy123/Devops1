7c4f1798d9a80b7ecd9f3b232f832a24d4170473
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-12 09:01:22 +1000

    HEY

--- project/components/mongodb.sh
+++ project/components/mongodb.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-souruce components/common.sh
+source components/common.sh
 
-PRINT "SET UP MONGODB"
+
+PRINT "Setup MongoDB Repository"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-STAT $? "SET UP MOGODB REPO"
+STAT $? "Setup MongoDB repository"
