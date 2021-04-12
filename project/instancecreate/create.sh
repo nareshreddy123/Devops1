5a2ca178d417bf7fcd388eae3d26a0733d3cc656
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-12 15:17:36 +1000

    HEY

--- project/instancecreate/create.sh
+++ project/instancecreate/create.sh
@@ -11,4 +11,14 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 
 fi
-  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+
+Instance_Create() {
+  COMPONENT=$1
+  INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
+  STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Name | xargs)
+  if [ -z "${INSTANCE_EXISTS}" -o "$STATE" == "terminated"  ]; then
+    aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+  else
+    echo "Instance ${COMPONENT} already exists"
+  fi
+}
