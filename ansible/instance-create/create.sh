d8b59b3560f911ce59a254519922e5b81c46c1bf
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-17 11:48:42 +1000

    Hi this my first programme1

--- ansible/instance-create/create.sh
+++ ansible/instance-create/create.sh
@@ -25,6 +25,8 @@ Instance_Create() {
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z0449177GO3AQK4VZZ1H --change-batch file:///tmp/record.json
+  sed -i -e "/${COMPONENT}/ d" ../inventory
+  echo "${IPADDRESS} APP=${COMPONENT}" >>../inventory
 }
 
 if [ "$1" == "all" ]; then
@@ -34,3 +36,4 @@ if [ "$1" == "all" ]; then
 else
   Instance_Create $1
 fi
+
